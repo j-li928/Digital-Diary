ff1a2953a6392002a4870cf2c3d437617a384589
j-li928 <jessicajli507@gmail.com>
2026-05-10 19:32:36 -0500

    Initialize journal script with shebang and features
    Added a shebang and improved journal script functionality.

--- scripts/journal.sh
+++ scripts/journal.sh
@@ -1,1 +1,30 @@
+#!/bin/bash
+#This is a shebang. It tells Linux which interpreter to use.
+#!/bin/bash is the standard linux command interpreter  
+
+
+
+#make a folder for year and month
+mkdir -p ~/journals/$(date +%Y)/$(date +%m)
+# S() means run this command and put the result here
+
+
+#-e tells echo to interpret escape codes
+#\e[32 switches the color
+#\e[0m resets back to normal color 
+echo -e "\e[36m Opening journal...Happy Journaling :)\e[0m"
+
+sleep 3
+
+JOURNAL=~/journals/$(date +%Y)/$(date +%m)/$(date +%Y-%m-%d).txt
+
+micro $JOURNAL
+#open a text file with todays date 
+
+
+echo "Word Count: $(wc -w < $JOURNAL)"
+
+echo "Journal Closed. Thanks for your thoughts!"
+sleep 3
+ 
 
